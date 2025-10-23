2b0c94682d579ccbf4c3224600323ce83df2eb48
Stanislav Bashkyrtsev <stanislav.bashkyrtsev@elsci.io>
2025-10-23 13:34:51 +0200

    (rs) Pass [u8] instead of str in benchmark
    To make it comparable with C.

--- src/main/rust/cli.rs
+++ src/main/rust/cli.rs
@@ -15,7 +15,7 @@ fn main() {
     let filepath = &args[1];
     let repeats = 50;
     let content = fs::read_to_string(filepath).expect(&format!("Couldn't read {filepath}"));
-    let lines: Vec<&str> = content.split("\n").collect();
+    let lines: Vec<&[u8]> = content.split("\n").map(|l|l.as_bytes()).collect();
     let mf_cnt = repeats * lines.len();
 
     let mut start = Instant::now();
@@ -29,11 +29,11 @@ fn main() {
              (mf_cnt as f64 / elapsed.as_secs_f64()) as u32);
 }
 
-fn parse_mfs(mfs: &Vec<&str>, n: usize) -> u32 {
+fn parse_mfs(mfs: &Vec<&[u8]>, n: usize) -> u32 {
     let mut hcount: u32 = 0;
     for _ in 0..n {
         for mf in mfs {
-            hcount += mf_parser::parse_mf(mf).unwrap().counts[0];
+            hcount += mf_parser::parse_mf_ascii_chunk(mf, 0, mf.len()).unwrap().counts[0];
         }
     }
     hcount // return something so that this isn't optimized out

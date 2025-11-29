62ecda513bfb57cbc60825d7e1684dae54c18c28
Stanislav Bashkyrtsev <stanislav.bashkyrtsev@elsci.io>
2025-11-29 15:14:20 +0200

    (asm) Implement getElementBySymbol_short

--- src/main/c/mf_parser.armv8.asm
+++ src/main/c/mf_parser.armv8.asm
@@ -6,6 +6,7 @@
 .global _MfParser_new
 .global _MfParser_destroy
 .global _MfParser_parseSanitized
+.global _ptable_getElementBySymbol_short
 
 .data
     ; struct MfParser field offsets and so on:
@@ -26,6 +27,29 @@
     .equ ChemikazeError_SIZE, 16 ; char *msg
 
     ChemikazeError_EMPTY_MOL_MSG: .asciz "Empty Molecular Formula"
+
+    PTABLE_ELEMENTHASH_TO_ELEMENT: ; hash(char[2]) -> ChemicalElement, see periodic_table.c
+        .byte 0,0,55,83,38,0,0,9,0,0,20,0,50,21,0,15,23,0,0,8,0,0,0,0,0,0,53,43,0,0,13,0
+        .byte 74,0,0,69,0,0,27,84,0,0,0,0,0,0,0,64,0,0,42,0,0,0,0,0,0,16,0,0,0,0,0,0
+        .byte 0,0,0,0,0,0,0,0,0,65,0,0,0,36,0,0,0,0,0,0,0,10,0,0,0,0,0,0,0,52,0,0
+        .byte 0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1
+        .byte 0,47,0,0,0,67,59,68,76,63,0,0,79,29,0,72,4,0,0,44,0,0,0,0,0,0,0,0,0,0,0,0
+        .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,25,0,0,0,26,58,0,0,0,0,0,0,0,0
+        .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,40,0,0,41,0,0,0,0,0,34,0,0,62,57,0,0,70
+        .byte 0,0,54,0,73,0,0,0,0,0,0,0,0,0,0,0,0,80,77,0,17,0,0,0,0,0,0,0,0,48,0,0
+        .byte 0,0,0,78,0,0,0,0,71,0,0,51,24,0,19,31,37,0,0,0,0,0,0,0,7,0,0,0,0,0,0,0
+        .byte 0,0,0,0,0,0,0,12,0,0,0,0,0,0,0,0,0,0,0,0,0,0,14,0,0,0,0,0,0,0,39,22
+        .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,33,0,0,0,0,0,0,0,49,0,0
+        .byte 0,0,0,0,0,0,0,0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0
+        .byte 0,0,0,0,0,0,0,61,0,0,0,0,0,82,66,75,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        .byte 0,45,56,0,0,0,30,0,0,0,28,0,18,46,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,35,5,0
+        .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,0,0,0,0,0,0,0,0,0,0,0,11,0,0,0,0
+        .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,60,0,0,0,0,0,0,0,0,0,0,0,81,0,0,0,0,0,0
+    .equ PTABLE_ELEMENTHASH_TO_ELEMENT_LEN, 512
+    .equ PTABLE_ELEMENTHASH_TO_ELEMENT_MASK, 511
+
+    MfParser_readSymbolsAndCoeffs__bigLetter_name: .asciz "MfParser_readSymbolsAndCoeffs__bigLetter\n"
+    MfParser_consumeSymbolAndCoeff_name: .asciz "MfParser_consumeSymbolAndCoeff\n"
 .text
 
 _MfParser_new:
@@ -101,8 +125,12 @@ _MfParser_parseSanitized:
         mov fp, sp
     stp x19, x20, [sp, -16]!
         mov x19, x3 ; ChemikazeError* to be optionally filled
+        mov x20, x4
     cmp x1, x2
         b.hs _MfParser_parseSanitized__emptyMfError
+    mov x0, x1
+        mov x1, x2
+        bl MfParser_readSymbolsAndCoeffs
 _MfParser_parseSanitized__out:
     ldp x19, x20, [sp], 16
     ldp fp, lr, [sp], 16
@@ -115,6 +143,57 @@ _MfParser_parseSanitized__emptyMfError:
     str x0, [x19] ; return ChemikazeError*
     mov x0, 0 ; return null
     b _MfParser_parseSanitized__out
+
+; @param [x0 -> x19] const char *mf - start of the MF
+; @param [x1 -> x20] const char *mfEnd - exclusive
+; @param ChemElement *elements
+; @param unsigned *coeff,
+; @param ChemikazeError **error
+MfParser_readSymbolsAndCoeffs:
+    stp fp, lr, [sp, -16]!
+        mov fp, sp
+    stp x19, x20, [sp, -16]!
+        mov x19, x0
+        mov x20, x1
+    stp x21, x22, [sp, -16]!
+MfParser_readSymbolsAndCoeffs__loop:
+    cmp x19, x20
+    b.eq MfParser_readSymbolsAndCoeffs__out
+    ldrb w21, [x19], 1 ; isBigLetter?
+        sub w22, w21, 'A'
+            cmp w22, 26
+                b.lo MfParser_readSymbolsAndCoeffs__bigLetter
+        ; TODO: else if (isPunctuation(*i) || isDigit(*i)), and then the error case
+    add x19, x19, 1
+    b MfParser_readSymbolsAndCoeffs__loop
+MfParser_readSymbolsAndCoeffs__bigLetter:
+    adrp x0, MfParser_readSymbolsAndCoeffs__bigLetter_name@page
+        add x0, x0, MfParser_readSymbolsAndCoeffs__bigLetter_name@pageoff
+        bl _printf
+    bl MfParser_consumeSymbolAndCoeff
+    mov x0, 16
+    b MfParser_readSymbolsAndCoeffs__loop
+MfParser_readSymbolsAndCoeffs__out:
+    ldp x21, x22, [sp], 16
+    ldp x19, x20, [sp], 16
+    ldp fp, lr, [sp], 16
+    ret
+
+; @param const char *mf - start of the MF
+; @param const char **i - exclusive
+; @param const char *mfEnd - exclusive
+; @param [x3] ChemElement *resultElements
+; @param [x4] unsigned *resultCoeff,
+; @param ChemikazeError **error
+MfParser_consumeSymbolAndCoeff:
+    adrp x0, MfParser_consumeSymbolAndCoeff_name@page
+        add x0, x0, MfParser_consumeSymbolAndCoeff_name@pageoff
+        bl _printf
+    mov x5, 'H'
+    mov x6, 1
+    str x5, [x3]
+    str x6, [x4]
+    ret
 
 ; @param void **oldPointer
 ; @param size_t newLen
@@ -155,6 +234,19 @@ _ChemikazeError_new:
 
     ldp x19, x20, [sp], 16
     ldp fp, lr, [sp], 16
+    ret
+
+; @param short symbol, where byte#0 is the big letter, and byte#1 is the small letter or 0
+_ptable_getElementBySymbol_short:
+    and w1, w0, 0xFF
+    mov w2, 277
+    mul w3, w1, w2
+    ubfx w4, w0, 8, 8 ; take the 2nd byte
+    eor w5, w3, w4
+    and w6, w5, PTABLE_ELEMENTHASH_TO_ELEMENT_MASK
+    adrp x7, PTABLE_ELEMENTHASH_TO_ELEMENT@page
+    add x7, x7, PTABLE_ELEMENTHASH_TO_ELEMENT@pageoff
+    ldrb w0, [x7, x6]
     ret
 
 ; @param unsigned c
--- src/main/c/mf_parser_asm.c
+++ src/main/c/mf_parser_asm.c
@@ -22,8 +22,20 @@ struct MfParser {
 // extern MfParser* MfParser_new();
 // extern void MfParser_destroy(MfParser*);
 
+int log_if_error(ChemikazeError* error, AtomCounts* counts) {
+	if (counts == NULL) {
+		printf("Error code: %d\n", error->code);
+		if (error->msg != NULL)
+			printf("Error msg: %s\n", error->msg);
+		return 1;
+	}
+	return 0;
+}
+extern ChemElement ptable_getElementBySymbol_short(int symbol);
+
 int main() {
 	printf("Is numeric=%d\n", isBigLetter('c'));
+	printf("ptable_getElementBySymbol=%u\n", ptable_getElementBySymbol_short(('l' << 8) + 'C'));
 	MfParser *parser = MfParser_new();
 	printf("sizeof(MfParser)=%lu\n", sizeof(MfParser));
 	printf("parser->len=%lu\n", parser->len);
@@ -36,14 +48,17 @@ int main() {
 	printf("Destroying:\n");
 	MfParser_destroy(parser);
 
-	char mf[0];
+#define MF_LEN 4
+	char *mf = "H2O4";
 	ChemikazeError *error = NULL;
+
 	AtomCounts* counts = MfParser_parseSanitized(parser, mf, mf, &error);
-	if (counts == NULL) {
-		printf("Error code: %d\n", error->code);
-		if (error->msg != NULL)
-			printf("Error msg: %s\n", error->msg);
-		return error->code;
+	log_if_error(error, counts);
+
+	counts = MfParser_parseSanitized(parser, mf, mf+MF_LEN+1, &error);
+	if (!log_if_error(error, counts)) {
+		printf("Symbols: %p\n", counts);
 	}
+
 	return 0;
 }

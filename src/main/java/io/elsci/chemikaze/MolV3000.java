6f87538f2353ed23868feecae5a6634b2ec8a327
Stanislav Bashkyrtsev <stanislav.bashkyrtsev@elsci.io>
2026-05-02 10:24:55 +0300

    Add null/empty checks for MOL V3000

--- src/main/java/io/elsci/chemikaze/MolV3000.java
+++ src/main/java/io/elsci/chemikaze/MolV3000.java
@@ -4,9 +4,15 @@ import java.nio.charset.StandardCharsets;
 
 public class MolV3000 {
     public static Molecule readOne(String mol) {
+        if(mol == null || mol.isEmpty())
+            throw new InvalidChemStructureException(mol, "Can't parse empty chemical structure");
         return readOne(mol.getBytes(StandardCharsets.US_ASCII));
     }
     public static Molecule readOne(byte[] mol) {
+        if(mol == null || mol.length == 0)
+            throw new InvalidChemStructureException("Can't parse empty chemical structure");
+        if(mol.length < 178) // all the essential blocks like BEGIN CTAB, BEGIN ATOMS combined
+            throw new InvalidChemStructureException(new String(mol, StandardCharsets.UTF_8), "Is not a proper MOL V3000 format");
         return null;
     }
 }
--- src/test/java/io/elsci/chemikaze/MolV3000Test.java
+++ src/test/java/io/elsci/chemikaze/MolV3000Test.java
@@ -4,6 +4,8 @@ package io.elsci.chemikaze;
 import org.junit.Test;
 
 import static io.elsci.chemikaze.CdkUtil.assertMoleculesEqual;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class MolV3000Test {
     @Test
@@ -11,5 +13,17 @@ public class MolV3000Test {
         String mol = IoUtils.getStringFromClasspath("molecules/methane.ketcher.molv3000");
         Molecule m = MolV3000.readOne(mol);
         assertMoleculesEqual(CdkUtil.fromMolV3000(mol), m);
+    }
+    @Test
+    @SuppressWarnings("DataFlowIssue")
+    public void errsIfStructureIsEmtpy() {
+        Exception e = assertThrows(InvalidChemStructureException.class, () -> MolV3000.readOne((String) null));
+        assertEquals("Can't parse empty chemical structure", e.getMessage());
+        assertThrows(InvalidChemStructureException.class, () -> MolV3000.readOne(""));
+        assertEquals("Can't parse empty chemical structure", e.getMessage());
+        assertThrows(InvalidChemStructureException.class, () -> MolV3000.readOne((byte[]) null));
+        assertEquals("Can't parse empty chemical structure", e.getMessage());
+        assertThrows(InvalidChemStructureException.class, () -> MolV3000.readOne(new byte[0]));
+        assertEquals("Can't parse empty chemical structure", e.getMessage());
     }
 }

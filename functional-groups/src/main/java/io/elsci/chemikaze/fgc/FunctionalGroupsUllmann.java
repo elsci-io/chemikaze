9c223aaf8c68034e03f8448a95f482042bb367ab
Stanislav Bashkyrtsev <stanislav.bashkyrtsev@elsci.io>
2026-08-23 13:29:16 +0300

    Ullmann substructure search - the initial DFS w/o matching
    Just the skeleton of DFS, w/o the actual matching.

--- functional-groups/src/main/java/io/elsci/chemikaze/fgc/FunctionalGroupsUllmann.java
+++ functional-groups/src/main/java/io/elsci/chemikaze/fgc/FunctionalGroupsUllmann.java
@@ -2,8 +2,52 @@ package io.elsci.chemikaze.fgc;
 
 import io.elsci.chemikaze.core.Molecule;
 
+import java.util.Arrays;
+
+// TODO:
+//  - Do the actual matching of elements and filling the connection map
+//  - Query isn't necessarily just a structure, it may be something more sophisticated like match any element on
+//    the list or match N that has some specific bond number / type
+//
 public class FunctionalGroupsUllmann implements FunctionalGroups {
+    private static final int UNMAPPED = -1;
+
     public int[][] match(Molecule target, Molecule query) {
+        int queryElement = query.getAtom(0);
+        for (int targetAtom = 0; targetAtom < target.getAtomCnt(); targetAtom++) {
+            if (queryElement != target.getAtom(targetAtom))
+                continue;
+            int[] atommap = new int[query.getAtomCnt()];
+            Arrays.fill(atommap, UNMAPPED);
+            dfs(target, targetAtom, query, 0, atommap);
+        }
         return new int[0][];
+    }
+
+    /**
+     *
+     * @param target molecule we search in
+     * @param targetatom the atom of the target molecule we're currently navigating from
+     * @param query the substructure that we're looking for
+     * @param queryatom the atom of the substructure we're currently navigating from
+     * @param atommap the result of the mapping between the target molecule and the substructure. The position (index)
+     *                of the element represent the atom index of the query, while the value is the index of the target
+     *                atom. It's filled with -1 initially, and as we find more atoms that match the query we fill more
+     *                indices.
+     */
+    private void dfs(Molecule target, int targetatom, Molecule query, int queryatom, int[] atommap) {
+        atommap[queryatom] = targetatom;
+        System.out.println(queryatom);
+        int bondCnt = query.getBondCnt(queryatom);
+        for (int b = 0; b < bondCnt; b++) {
+            int connectedAtom = query.getConnectedAtom(queryatom, b);
+            if (atommap[connectedAtom] >= 0) // has been visited and mapped already
+                continue;
+            for (int targetbond = 0; targetbond < target.getBondCnt(targetatom); targetbond++)
+                 // todo:
+                 //  - check if the target atom has been visited and mapped already
+                 //  - do the actual match between the queryelement and targetelement
+                 dfs(target, target.getConnectedAtom(targetatom, targetbond), query, connectedAtom, atommap);
+        }
     }
 }
--- functional-groups/src/test/java/io/elsci/chemikaze/fgc/FunctionalGroupsUllmannTest.java
+++ functional-groups/src/test/java/io/elsci/chemikaze/fgc/FunctionalGroupsUllmannTest.java
@@ -9,10 +9,11 @@ public class FunctionalGroupsUllmannTest {
     @Test
     public void findsOneAlocohol() {
         assertArrayEquals(new int[][]{{2, 1}}, match("CCO", "CO"));
+//        assertArrayEquals(new int[][]{{2, 1}}, match("CCCO", "CO"));
     }
 
     private static int[][] match(String target, String query) {
         Smiles parser = new Smiles();
-        return FunctionalGroups.create().match(parser.readOne(query), parser.readOne(target));
+        return FunctionalGroups.create().match(parser.readOne(target), parser.readOne(query));
     }
 }
